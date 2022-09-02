26bc34e7dda1ff8804348f81bed90868fe71a9ba
Sheldon Fam <sheldonfam95@gmail.com>
2022-09-02 22:43:27 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Note from "./components/Note";
+import CreateNote from "./components/CreateNote";
 
-function App() {
+function App(props) {
+  const [notes, setNotes] = useState([]);
+
+  const addNote = (newNote) => {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote];
+    });
+  };
+
+  const deleteNote = (id) => {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <CreateNote onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
     </div>
   );
 }
---
+++ src/components/CreateNote.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import Zoom from "@mui/material/Zoom";
+import AddIcon from "@mui/icons-material/Add";
+
+const CreateNote = (props) => {
+  const [createNote, setCreateNote] = useState({ title: "", content: "" });
+  const [Expanded, setExpanded] = useState(false);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setCreateNote((prevNote) => {
+      return { ...prevNote, [name]: value };
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.onAdd(createNote);
+    setCreateNote({
+      title: "",
+      content: "",
+    });
+    setExpanded(false);
+  };
+
+  const expand = () => {
+    setExpanded(true);
+  };
+
+  return (
+    <div>
+      <form className="create-note">
+        {Expanded && (
+          <input
+            type="text"
+            name="title"
+            placeholder="Title"
+            onChange={handleChange}
+            value={createNote.title}
+          />
+        )}
+
+        <textarea
+          name="content"
+          id=""
+          rows={Expanded ? 3 : 1}
+          placeholder="Note"
+          onClick={expand}
+          onChange={handleChange}
+          value={createNote.content}
+        ></textarea>
+        <Zoom in={Expanded}>
+          <button onClick={handleSubmit}>
+            <AddIcon />
+          </button>
+        </Zoom>
+      </form>
+    </div>
+  );
+};
+
+export default CreateNote;
